db880021d2d7285244ced2c17928e70e826c2e10
Mark-Johannes Becker <mjb@marb.ec>
2015-07-13 00:05:40 +0200

    Go test in unit format added

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ ENV BUILDPATH src
 ENV GOOS linux
 ENV GOARCH amd64
 
+RUN go get -u github.com/jstemmer/go-junit-report
+
 COPY build.sh /build.sh
 RUN chmod +x /build.sh
 
--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ cd $BUILDPATH
 echo "GET DEPENDENCIES"
 go get
 
+echo "GO TEST"
+go test -v | go-junit-report > report.xml
+
 echo "BUILD GO PROJECT"
 echo "GOOS is $GOOS"
 echo "GOARCH is $GOARCH"
