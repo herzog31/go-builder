4acd71b22cd1504ba33435f0f1d7fb72386a9eb7
Mark-Johannes Becker <mjb@marb.ec>
2015-07-12 23:13:42 +0200

    go get and go build added

--- build.sh
+++ build.sh
@@ -5,4 +5,10 @@ echo "BUILDPATH is $GOPATH/$BUILDPATH"
 cd $GOPATH
 cd $BUILDPATH
 
+echo "GET DEPENDENCIES"
+go get
+
+echo "BUILD GO PROJECT"
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo .
+
 ls -R
